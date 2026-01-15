814b6f3ee919be90212461c254fddeb26b1d22f7
Tarun <thakurtarun1012@gamil.com>
2026-01-15 15:50:21 +0530

    fix babel plugin

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:8080",
-    specPattern: "cypress/integration/tests/test.spec.js",
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     supportFile: false,
   },
 });
