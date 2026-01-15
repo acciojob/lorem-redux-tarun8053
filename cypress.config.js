d40b1b2ab43a7f09fa8f21e601b0a45896559354
Tarun <thakurtarun1012@gamil.com>
2026-01-15 15:13:15 +0530

    fix baseUrl port

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:8081",
+    baseUrl: "http://localhost:8080",
     specPattern: "cypress/integration/tests/test.spec.js",
     supportFile: false,
   },
